967c2e7d8e0ccfb81d440d7a58f4398921947183
yaxollum <yaxollum@gmail.com>
2020-05-23 08:09:39 -0400

    added ChangeVariable

---
+++ include/StackedBlock/VariableOperation/ChangeVariable.h
@@ -0,0 +1,16 @@
+#include <memory>
+#include "StackedBlock/StackedBlock.h"
+
+class Variable;
+class NestedBlock;
+
+class ChangeVariable : public StackedBlock
+{
+public:
+    ChangeVariable(std::shared_ptr<Variable> var,
+        const std::shared_ptr<NestedBlock> val);
+    void exec() const override;
+private:
+    std::shared_ptr<Variable> variable;
+    const std::shared_ptr<NestedBlock> value;
+};
---
+++ src/StackedBlock/VariableOperation/ChangeVariable.cc
@@ -0,0 +1,14 @@
+#include <memory>
+#include "StackedBlock/VariableOperation/ChangeVariable.h"
+#include "NestedBlock/RoundBlock/Variable.h"
+#include "NestedBlock/NestedBlock.h"
+
+ChangeVariable::ChangeVariable(std::shared_ptr<Variable> var,
+    const std::shared_ptr<NestedBlock> val)
+    : variable(var),value(val) {}
+
+void ChangeVariable::exec() const
+{
+    variable->setValue(std::to_string(variable->getDouble()+
+        value->getDouble()));
+}
