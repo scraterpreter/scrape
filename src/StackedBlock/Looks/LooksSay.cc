4e11b904e2be65fc2f71f8edfd69a59da177d5ff
Paul Lee <enigma@enigmatic.network>
2020-05-28 00:13:24 -0400

    modify LooksSay and LooksThink blocks to remove trailing zeros and the decimal point when printing numbers

--- src/StackedBlock/Looks/LooksSay.cc
+++ src/StackedBlock/Looks/LooksSay.cc
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <regex>
 #include "NestedBlock/NestedBlock.h"
 #include "StackedBlock/Looks/LooksSay.h"
 
 void LooksSay::exec() const
 {
-    std::cout << val->getValue() << '\n';
+    if (std::regex_match (val->getValue(), std::regex("(\\d*)\\.?(\\d*)"))) {
+        std::cout << std::stod(val->getValue()) << '\n';
+    } else {
+        std::cout << val->getValue() << '\n';
+    }
 }
 LooksSay::LooksSay(const std::shared_ptr<NestedBlock> v) : val(v) {}
--- src/StackedBlock/Looks/LooksThink.cc
+++ src/StackedBlock/Looks/LooksThink.cc
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <regex>
 #include "NestedBlock/NestedBlock.h"
 #include "StackedBlock/Looks/LooksThink.h"
 
 void LooksThink::exec() const
 {
-    std::cout << val->getValue();
+    if (std::regex_match (val->getValue(), std::regex("(\\d*)\\.?(\\d*)"))) {
+        std::cout << std::stod(val->getValue());
+    } else {
+        std::cout << val->getValue();
+    }
 }
 LooksThink::LooksThink(const std::shared_ptr<NestedBlock> v) : val(v) {}
